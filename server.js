9aac06ebe71fe90503cd69b7a96df3e295d2abad
ram-togglehead <ramprasad@togglehead.in>
2025-11-22 01:38:34 +0530

    Server Port NO Changed

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const PORT = 9084;
+const PORT = 9089;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
